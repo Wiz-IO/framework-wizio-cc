ad7124e0e8b9cbc2a79f57129c76d42e2cbe2b7d
Georgi Angelov <the.wizarda@gmail.com>
2021-08-09 13:53:16 +0300

    Add files via upload

---
+++ arduino/libraries/CC2652/OT_RADIO/radio.h
@@ -0,0 +1,52 @@
+#include <stdint.h>
+#include "ot_radio.h"
+
+class Radio
+{
+public:
+    Radio() { cc2652RadioInit(); }
+
+    /* for receive */
+    inline void SetPanId(uint16_t aPanid) { otPlatRadioSetPanId(aPanid); }
+
+    /* for receive */
+    inline void SetShortAddress(uint16_t aAddress) { otPlatRadioSetShortAddress(aAddress); }
+
+    inline int SetTransmitPower(int8_t aPower) { return otPlatRadioSetTransmitPower(aPower); }
+
+    int enable() { return otPlatRadioEnable(); }
+
+    int disable() { return otPlatRadioDisable(); }
+
+    int receive(uint8_t aChannel = 11) { return otPlatRadioReceive(aChannel); }
+
+    /*auto add length & crc*/
+    int send_raw(uint8_t *buffer, uint8_t size)
+    {
+        if (otPlatRadioIsEnabled() && buffer && size < 128 /* max ? */)
+        {
+            otRadioFrame frame;
+            frame.mPsdu = buffer;
+            frame.mLength = size + 2; /* 2 bytes of CRC placeholder, removing, because we generate that in hardware */
+            return otPlatRadioTransmit(&frame);
+        }
+        return -1;
+    }
+
+    void loop()
+    {
+        cc2652RadioProcess();
+    }
+
+    bool begin(uint16_t aPanid, uint16_t aAddress, uint8_t aChannel = 11)
+    {
+        otPlatRadioSetPanId(aPanid);          /* for receive */
+        otPlatRadioSetShortAddress(aAddress); /* for receive */
+        otPlatRadioSetTransmitPower(0);       /* error ?! */
+        if (OT_ERROR_NONE == otPlatRadioEnable())
+            if (OT_ERROR_NONE == otPlatRadioReceive(aChannel))
+                return true;
+        //::printf("[ERROR] Radio.begin()\n");
+        return false;
+    }
+};
