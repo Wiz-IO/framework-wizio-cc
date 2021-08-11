9d92e27dd164f8c0166ec182bf3e7695e684fec7
Georgi Angelov <the.wizarda@gmail.com>
2021-08-11 09:07:50 +0300

    Add files via upload

---
+++ arduino/libraries/CC2652/OT_RADIO/ieee_ot_radio.h
@@ -0,0 +1,111 @@
+////////////////////////////////////////////////////////////////////////////////////////
+//
+//      2021 Georgi Angelov
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdint.h>
+#include "ot_radio.h"
+
+/* CALLBACKS
+
+void otPlatRadioEnergyScanDone(int8_t aEnergyScanMaxRssi)
+{
+    printf("[RADIO] %s()\n", __func__);
+}
+
+void otPlatRadioReceiveDone(otRadioFrame *aFrame, otError aError)
+{
+    printf("[RADIO] %s()\n", __func__);
+}
+
+void otPlatRadioTxStarted(otRadioFrame *aFrame)
+{
+    printf("[RADIO] %s()\n", __func__);
+}
+
+void otPlatRadioTxDone(otRadioFrame *aFrame, otRadioFrame *aAckFrame, otError aError)
+{
+    printf("[RADIO] %s( %d )\n", __func__, (int)aError);
+}
+
+*/
+
+class Radio
+{
+
+public:
+    Radio() { cc2652RadioInit(); }
+    ~Radio() { otPlatRadioDisable(); }
+
+    inline void SetPanId(uint16_t aPanid) { otPlatRadioSetPanId(aPanid); }
+
+    inline void SetShortAddress(uint16_t aAddress) { otPlatRadioSetShortAddress(aAddress); }
+
+    inline int SetTransmitPower(int8_t aPower) { return otPlatRadioSetTransmitPower(aPower); }
+
+    inline int enable() { return otPlatRadioEnable(); }
+
+    inline int disable() { return otPlatRadioDisable(); }
+
+    /* 
+        callback: otPlatRadioReceiveDone() 
+    */
+    int receive(uint8_t aChannel = 11) { return otPlatRadioReceive(aChannel); }
+
+    inline void loop() { cc2652RadioProcess(); }
+
+    /*
+        transmit raw data
+        auto add length & crc
+        callback: otPlatRadioTxStarted() &  otPlatRadioTxDone()
+    */
+    int transmit(uint8_t *buffer, uint8_t size)
+    {
+        if (buffer && size < 128 /* max ? */)
+        {
+            otRadioFrame frame;
+            frame.mPsdu = buffer;
+            frame.mLength = size + 2; /* 2 bytes of CRC placeholder, removing, because we generate that in hardware */
+            return otPlatRadioTransmit(&frame);
+        }
+        return -1;
+    }
+
+    bool begin(uint16_t aPanid, uint16_t aAddress, int8_t aPower, uint8_t aChannel = 11)
+    {
+        if (false == otPlatRadioIsEnabled())
+        {
+            otPlatRadioSetPanId(aPanid);
+            otPlatRadioSetShortAddress(aAddress);
+            if (OT_ERROR_NONE == otPlatRadioSetTransmitPower(aPower))
+            {
+                if (OT_ERROR_NONE == otPlatRadioEnable())                 // power on
+                    return OT_ERROR_NONE == otPlatRadioReceive(aChannel); // goto rx mode
+            }
+        }
+        return false;
+    }
+
+    bool begin(uint8_t aChannel = 11)
+    {
+        if (false == otPlatRadioIsEnabled())
+        {
+            if (OT_ERROR_NONE == otPlatRadioEnable())                 // power on
+                return OT_ERROR_NONE == otPlatRadioReceive(aChannel); // goto rx mode
+        }
+        return false;
+    }
+};
