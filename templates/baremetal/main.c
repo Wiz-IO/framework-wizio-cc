7d4c9c0ddfd0e80fcab5ab5c0b40e4cda26fa7ce
Georgi Angelov <the.wizarda@gmail.com>
2021-10-20 12:23:34 +0300

    Add files via upload

--- templates/baremetal/main.c
+++ templates/baremetal/main.c
@@ -26,19 +26,21 @@ void powerOn(uint32_t domain, uint32_t periph, bool sleep, bool deep_sleep)
     }
 }
 
-int main(void)
+void setup(void)
 {
     powerOn(PRCM_DOMAIN_SERIAL, PRCM_PERIPH_UART0, 0, 0);       // power on uart0
     IOCPinTypeUart(UART0_BASE, 2, 3, IOID_UNUSED, IOID_UNUSED); // pin as rx, tx
     UARTConfigSetExpClk(UART0_BASE, SysCtrlClockGet(), 115200, UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE);
     UARTEnable(UART0_BASE);
-    retarget_uart(UART0_BASE); // printf
+    retarget_uart(UART0_BASE); // use stdio printf
     printf("[APP] Hello World\n");
-
     powerOn(PRCM_DOMAIN_PERIPH, PRCM_PERIPH_GPIO, 0, 0);           // power on gpio
     IOCPortConfigureSet(LED_GREEN, IOC_PORT_GPIO, IOC_STD_OUTPUT); // pin as gpio
     GPIO_setOutputEnableDio(LED_GREEN, GPIO_OUTPUT_ENABLE);        // pin dir output
+}
 
+void loop(void)
+{
     while (1)
     {
         printf("LOOP ");
@@ -47,5 +49,11 @@ int main(void)
         GPIO_writeDio(LED_GREEN, 0);
         CPUdelay(10000000);
     }
+}
+
+int main(void)
+{
+    setup();
+    loop();
     return -1;
 }
